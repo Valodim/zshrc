aa81dc35dc7f2b07a54a39b18049e5ac20f6ab5f
Valodim <valodim@mugenguild.com>
2011-08-06 02:20:25 +0200

    duplicate line in zstyles

--- custom/30-zstyle.zsh
+++ custom/30-zstyle.zsh
@@ -7,8 +7,6 @@ zstyle ':completion::complete:*' cache-path $ZSH/cache/
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' list-prompt '%SAt %p: Hit TAB for more, or the character to insert%s'
 zstyle ':completion:*' menu select=1 _complete _ignored _approximate
-zstyle -e ':completion:*:approximate:*' max-errors \
-    'reply=( $(( ($#PREFIX+$#SUFFIX)/2 )) numeric )'
 zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
 
 # Completion Styles
