4ec22835fc76a90934c768b2d5dd007b9e3eb7eb
Vincent Breitmoser <valodim@mugenguild.com>
2012-03-25 23:43:11 +0200

    consolidate exports and history

--- custom/30-export.zsh
+++ custom/30-export.zsh
@@ -8,5 +8,11 @@ export LESS_TERMCAP_so=$'\E[01;44;33m'
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[01;32m'
 
+export PAGER=less
+export LC_CTYPE=en_US.UTF-8
+
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='1;32'
+
 LOGCHECK=60
 WATCHFMT="[%B%t%b] %B%n%b has %a %B%l%b from %B%M%b"
--- custom/30-history.zsh
+++ custom/30-history.zsh
@@ -3,14 +3,7 @@ HISTFILE=$HOME/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt hist_ignore_dups # ignore duplication command history list
-setopt share_history # share command history data
+setopt hist_ignore_dups hist_expire_dups_first
+setopt hist_verify inc_append_history extended_history hist_ignore_space append_history
 
-setopt hist_verify
-setopt inc_append_history
-setopt extended_history
-setopt hist_expire_dups_first
-setopt hist_ignore_space
-
-setopt SHARE_HISTORY
-setopt APPEND_HISTORY
+unsetopt share_history
