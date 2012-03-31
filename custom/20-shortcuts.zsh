2744b0ead2635453d909bde2b146f8ea333a4022
Vincent Breitmoser <valodim@mugenguild.com>
2012-03-31 18:07:37 +0200

    add offliberty shortcut

--- custom/20-shortcuts.zsh
+++ custom/20-shortcuts.zsh
@@ -16,3 +16,33 @@ function mksub () {
         mv $i $d/${${i#$1}# #}
     done
 }
+
+# this is a curl wrapper for http://offliberty.com
+function offliberty() {
+    # stuff this is known to work with:
+    #  * mixcloud
+
+    # Usage
+    [[ $# != 1 ]] && echo "Usage: $0 URL" >&2 && return 1
+
+    # Retrieve data (thanks offliberty guys for making this as simple as it is :) )
+    data=$(curl --silent --data-urlencode track=$1 http://offliberty.com/off.php | grep 'download')
+    ret=$?
+
+    # Got an error :(
+    if (( $ret != 0 )); then
+        echo 'Error with curl!'
+        return $ret
+    fi
+
+    # Match the url
+    if [[ $data =~ '.*"(http://[a-zA-Z0-9_/.-]*)" .*' ]]; then
+        echo $match
+    else
+        echo 'No url found in answer!' >&2
+        return 2
+    fi
+
+    # todo: offliberty sometimes returns mp3, sometimes m4a.. maybe add an option to force one filetype
+
+}
