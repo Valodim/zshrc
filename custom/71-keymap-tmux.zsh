c8be8491a2f7f0f23ec19ffaffd8efbbe397a23f
Vincent Breitmoser <valodim@mugenguild.com>
2012-04-15 20:54:12 +0200

    add `s and `v tmux bindings

---
+++ custom/71-keymap-tmux.zsh
@@ -0,0 +1,25 @@
+# tmux stuff
+function ztmux-split-vertical() {
+    # are we even inside a screen/tmux? if so, just return
+    [[ $TERM == *screen* ]] && return
+
+    # this does not work with multiline editing, but I don't use that anyways
+    BUFFER=" tmux new-session 'ZSH_INIT_BUFFER=\"$BUFFER\" zsh' \; split-window -d -h"
+    zle accept-line
+}
+# we call it ztmux to avoid completion clashes
+zle -N ztmux-split-vertical
+
+# tmux stuff
+function ztmux-split-horizontal() {
+    # are we even inside a screen/tmux? if so, just return
+    [[ $TERM == *screen* ]] && return
+
+    BUFFER=" tmux new-session 'ZSH_INIT_BUFFER=\"$BUFFER\" zsh' \; split-window -d -v"
+    zle accept-line
+}
+# we call it ztmux to avoid completion clashes
+zle -N ztmux-split-horizontal
+
+bindkey '`v' ztmux-split-vertical
+bindkey '`s' ztmux-split-horizontal
---
+++ custom/99-mybuffer.zsh
@@ -0,0 +1,2 @@
+# this sets an initial buffer from environment
+[[ -n $ZSH_INIT_BUFFER ]] && print -z $ZSH_INIT_BUFFER && unset $ZSH_INIT_BUFFER
