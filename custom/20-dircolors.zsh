9ebdd0d501f562e2dedfb29980bd98f6ac778988
Vincent Breitmoser <valodim@mugenguild.com>
2012-04-16 03:32:07 +0200

    don't load dircolors for <256 color TERMs

--- custom/20-dircolors.zsh
+++ custom/20-dircolors.zsh
@@ -1,4 +1,6 @@
 
-# dircolors. not a zstyle per se, but still kind of a style thing
-eval $( dircolors -b $ZSH/subs/LS_COLORS/LS_COLORS )
-
+# only available for 256 color terminals
+if (( $(echotc Co) == 256 )); then
+    # load trapd00r's LS_COLORS things
+    eval $( dircolors -b $ZSH/subs/LS_COLORS/LS_COLORS )
+fi
