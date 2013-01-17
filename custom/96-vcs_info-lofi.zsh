dbea37ae2178db86098d51c3cca21608cb72d67d
Vincent Breitmoser <valodim@mugenguild.com>
2013-01-17 02:08:31 +0100

    use per-directory lofi, complete with keybinding

--- custom/96-vcs_info-lofi.zsh
+++ custom/96-vcs_info-lofi.zsh
@@ -1,4 +1,4 @@
-# this is a hook which stops vcs_info right after detection step.
+# this is a hook which stops vcs_info right after the vcs detection step.
 #
 # this allows displaying a piece of information that the current PWD is inside
 # a vcs repository and its type, without loading the heavyweight vcs data.
@@ -10,16 +10,19 @@
 # zstyle ':vcs_info:*:bleep:*'   lofiformats   "%3~ %s"
 # zstyle ':vcs_info:git:bleep:*' lofiformats   "%3~ ±"
 #
+# this feature was hardly tested with anything besides git, and probably has
+# bugs. use at your own risk.
+#
 +vi-lofi () {
 
     # locals
-    local msg
+    local msg basedir subdir
     local -i i
     local -xA hook_com
     local -xa msgs
 
     # lofi mode enabled?
-    if ! zstyle -t ":vcs_info:${vcs}:${usercontext}:${rrn}" lofi ; then
+    if ! zstyle -t ":vcs_info:${vcs}:${usercontext}:${PWD}" lofi ; then
         # if not, just leave.
         return 0
     fi
@@ -30,11 +33,23 @@
     # if no lofi styles are set, better chicken out here
     (( $#msgs == 0 )) && return 0
 
+    # git has the only detect mechanism that does not set the basedir. sheesh.
+    if (( $+vcs_comm[gitdir] )); then
+        basedir=${PWD%/${$( ${vcs_comm[cmd]} rev-parse --show-prefix )%/##}}
+    elif (( $+vcs_comm[basedir] )); then
+        basedir=$vcs_comm[basedir]
+    else
+        # no set? huh. just assume it's PWD, then...
+        basedir=$PWD
+    fi
+    # get subdir within repo tree
+    subdir="$(VCS_INFO_reposub ${basedir})"
+
     # process the messages as usual, but with only the vcs name as %s
     (( ${#msgs} > maxexports )) && msgs[$(( maxexports + 1 )),-1]=()
     for i in {1..${#msgs}} ; do
         if VCS_INFO_hook "set-lofi-message" $(( $i - 1 )) "${msgs[$i]}"; then
-            zformat -f msg ${msgs[$i]} s:${vcs}
+            zformat -f msg ${msgs[$i]} s:${vcs} R:${basedir} S:${subdir}
             msgs[$i]=${msg}
         else
             msgs[$i]=${hook_com[message]}
@@ -49,5 +64,23 @@
     return 1
 }
 
+# function which disables lofi for this particular directory
+prompt-vcs-hifi-pwd () {
+    # disable lofi mode for this directory and all subdirs
+    zstyle ":vcs_info:*:*:$PWD*" lofi false
+
+    # if this is run as a widget, ...
+    if zle; then
+        # manually re-run all prompt precmd functions (so vcs_info is
+        # reevaluated as well), and reset prompt to update status.
+        for f in ${(M)precmd_functions#prompt_*_precmd}; $f
+        zle .reset-prompt
+    fi
+}
+zle -N prompt-vcs-hifi-pwd
+bindkey '^G^L' prompt-vcs-hifi-pwd
+
+# all directories are lofi by default
+zstyle ':vcs_info:*' lofi true
 # set the hook
 zstyle ':vcs_info-static_hooks:pre-get-data' hooks 'lofi'
