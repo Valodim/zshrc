72f3b2087b9baac0d4c8e28491b0ab482e480b91
Vincent Breitmoser <valodim@mugenguild.com>
2013-08-09 13:44:55 +0200

    vcs_info-lofi: various improvements
    - use hook_com, so hooks are compatible with regular set-message hooks
    - use vcs_info_hookadd
    - in the history hook, respect aliases which expand to git* as well
    - add hard zsh 4.3.12+ dependency (for hookadd, at least)

--- config/96-vcs_info-lofi.zsh
+++ config/96-vcs_info-lofi.zsh
@@ -1,22 +1,32 @@
+# a lot of vcs_info related stuff happened here
+autoload -U is-at-least
+is-at-least 4.3.12 || return
+
 # this is a hook which stops vcs_info right after the vcs detection step.
 #
 # this allows displaying a piece of information that the current PWD is inside
 # a vcs repository and its type, without loading the heavyweight vcs data.
 #
 # this hook uses the :vcs_info:vcsname:usercontext:* lofiformats style, where
-# only %s is replaced with the vcs name. appearance will typically be customized
-# like this, if you call vcs_info with the 'bleep' usercontext:
+# only %s, %S and %R are replaced as usual to vcs name, basedir and subdir
+# respectively. appearance will typically be customized like this, if you call
+# vcs_info with the 'bleep' usercontext:
 #
 # zstyle ':vcs_info:*:bleep:*'   lofiformats   "%3~ %s"
-# zstyle ':vcs_info:git:bleep:*' lofiformats   "%3~ ±"
+# zstyle ':vcs_info:git:bleep:*' lofiformats   "%S%%F{yellow}/%f%R ±"
+#
+# similarly to regular vcs_info's set-message mechanism, the set-lofi-message
+# hook is called which may be used to further influence these formats. for an
+# example, see:
+# https://github.com/Valodim/zshrc/blob/master/custom/96-vcs_info-disambiguate.zsh
 #
 # this feature was hardly tested with anything besides git, and probably has
 # bugs. use at your own risk.
-#
+
 +vi-lofi () {
 
     # locals
-    local msg basedir subdir
+    local msg
     local -i i
     local -xA hook_com
     local -xa msgs
@@ -35,21 +45,23 @@
 
     # git has the only detect mechanism that does not set the basedir. sheesh.
     if (( $+vcs_comm[gitdir] )); then
-        basedir=${PWD%/${$( ${vcs_comm[cmd]} rev-parse --show-prefix )%/##}}
-    elif (( $+vcs_comm[basedir] )); then
-        basedir=$vcs_comm[basedir]
+        hook_com[base]=${PWD%/${$( ${vcs_comm[cmd]} rev-parse --show-prefix )%/##}}
+    elif (( $+vcs_comm[base] )); then
+        hook_com[base]=$vcs_comm[basedir]
     else
         # no set? huh. just assume it's PWD, then...
-        basedir=$PWD
+        hook_com[base]=$PWD
     fi
-    # get subdir within repo tree
-    subdir="$(VCS_INFO_reposub ${basedir})"
+
+    # set up hook_com variables
+    hook_com[subdir]="$(VCS_INFO_reposub ${hook_com[base]})"
+    hook_com[vcs]=$vcs
 
     # process the messages as usual, but with only the vcs name as %s
     (( ${#msgs} > maxexports )) && msgs[$(( maxexports + 1 )),-1]=()
     for i in {1..${#msgs}} ; do
         if VCS_INFO_hook "set-lofi-message" $(( $i - 1 )) "${msgs[$i]}"; then
-            zformat -f msg ${msgs[$i]} s:${vcs} R:${basedir} S:${subdir}
+            zformat -f msg ${msgs[$i]} s:${hook_com[vcs]} R:${hook_com[base]} S:${hook_com[subdir]}
             msgs[$i]=${msg}
         else
             msgs[$i]=${hook_com[message]}
@@ -61,6 +73,7 @@
 
     # this is the value passed back up to vcs_info, which stops further processing
     ret=1
+    # don't process any other hooks, either
     return 1
 }
 
@@ -82,8 +95,10 @@ bindkey '^G^L' prompt-vcs-hifi-pwd
 
 # set lofi to false for current directory, if any kind of git command is run
 vcs-hifi-pwd-hook () {
-    if [[ $1 == git* ]] && zstyle -t ":vcs_info:*:*:$PWD" lofi; then
-        zstyle ":vcs_info:*:*:$PWD*" lofi false
+    local firstword=${${(z)1}[1]}
+    if [[ $firstword == git ]] || (( $+aliases[$firstword] )) && [[ $aliases[$firstword] == git* ]]; then
+        # set to false, if still true for this path
+        zstyle -t ":vcs_info:*:*:$PWD" lofi && zstyle ":vcs_info:*:*:$PWD*" lofi false
     fi
     return 0
 }
@@ -94,4 +109,5 @@ add-zsh-hook zshaddhistory vcs-hifi-pwd-hook
 # all directories are lofi by default
 zstyle ':vcs_info:*' lofi true
 # set the hook
-zstyle ':vcs_info-static_hooks:pre-get-data' hooks 'lofi'
+autoload -U vcs_info_hookadd
+vcs_info_hookadd pre-get-data lofi
