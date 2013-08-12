5129f6457bb6ba3fae97b1be6bdf64aa2bf42484
Vincent Breitmoser <valodim@mugenguild.com>
2013-08-13 01:51:50 +0200

    move some configs which work standalone into separate module folder

--- config/15-gistpaste.zsh
+++
@@ -1,7 +0,0 @@
-if [[ -e $ZSH/subs/gistpaste ]]; then
-    if [[ ! -x $ZSH/subs/gistpaste/gistpaste ]]; then
-        chmod +x $ZSH/subs/gistpaste/gistpaste
-    fi
-    export PATH=$PATH:$ZSH/subs/gistpaste
-    alias -g GP='| gistpaste'
-fi
