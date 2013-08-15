23d758fa417dd9f9fdfe6c8869d005eb386600a1
Vincent Breitmoser <valodim@mugenguild.com>
2013-08-15 13:36:00 +0200

    title: compatibility improvements

--- modules/50-title.zsh
+++ modules/50-title.zsh
@@ -1,8 +1,27 @@
 
 # try to give the window a meaningful title, at least some of the time
 
-# run some application, maybe?
+# use terminfo data to set title. this works with urxvt and probably others
+title-set-terminfo () {
+    print -nR "$terminfo[tsl]$1 ${*[2,$]}$terminfo[fsl]"
+}
+# xterm doesn't work with terminfo -> special treatment
+title-set-xterm () {
+    print -nR $'\033]0;'$1 ${*[2,$]}$'\007'
+}
+
+# set tab title in screen
+title-set-screen () {
+    # tab title only uses $1
+    print -nR $'\033k'$1$'\033\\'
+}
+
+# run some application, maybe? set title to "command (pwd)"
 title-preexec () {
+
+    # don't bother if nopony's listening anyways
+    (( $#title_functions > 0 )) || return
+
     local -a buf
     buf=( ${(z)1} )
 
@@ -11,7 +30,7 @@ title-preexec () {
 
     # first is a precommand? shift dis shit.
     local prefix
-    if (( $+precommands[(r)$buf[1]] )); then
+    while (( $+precommands[(r)$buf[1]] )); do
         if [[ $buf[1] == sudo ]]; then
 
             # show sudo as a prefix
@@ -26,29 +45,47 @@ title-preexec () {
         else
             shift buf
         fi
-    fi
+    done
 
     # only care for a couple of command-type types. "for" is hardly a useful title
     local typ=${"$(LC_ALL=C builtin whence -w $buf[1] 2>/dev/null)"#*: }
     [[ $typ == "function" || $typ == "command" || $typ == "alias" ]] || return
 
-    # set the title
-    print -nR $'\033]0;'"$prefix${buf[1]:t} (${(D)PWD})"$'\007'
-    # is this screen or tmux? set tab title there, too
-    if [[ $TERM == screen* ]]; then
-        print -nR $'\033k'"$prefix${buf[1]:t}"$'\033\\'
-    fi
+    local f
+    for f in $title_functions; do
+        $f "$prefix${buf[1]:t}" "(${(D)PWD})"
+    done
 
 }
 
-# back to the prompt
+# back to the prompt, title is "zsh (pwd)"
 title-precmd () {
-    print -nR $'\033]2;'"zsh (${(D)PWD})"$'\007'
-    if [[ $TERM == screen* ]]; then
-        print -nR $'\033k'"zsh"$'\033\\'
-    fi
+
+    local f
+    for f in $title_functions; do
+        $f zsh "(${(D)PWD})"
+    done
+
 }
 
+# array of functions to call for title setting
+typeset -aH title_functions
+
+# if we can get tsl from terminfo, that'd be perfect. works with urxvt.
+zmodload zsh/terminfo
+if (( $+terminfo[tsl] && $+terminfo[fsl] )); then
+    title_functions+=( title-set-terminfo )
+# otherwise, fallback for specific terminals (maybe expand this list?)
+elif [[ $TERM == xterm* ]]; then
+    title_functions+=( title-set-xterm )
+fi
+
+# set screen title independently
+if [[ $TERM == screen* ]]; then
+    title_functions+=( title-set-screen )
+fi
+
+# we do this regardless, to always provide title_functions functionality
 autoload -U add-zsh-hook
 add-zsh-hook preexec title-preexec
 add-zsh-hook precmd title-precmd
